2591be5b4f01be5beb3553eea9e2f558e2e7179a
Dmitry Latnikov <dlatnikov@griddynamics.com>
2013-09-23 17:03:25 +0400

    collectors docu

--- search/all_6d.js
+++ search/all_6d.js
@@ -1,7 +1,15 @@
 var searchData=
 [
   ['makedecision',['makeDecision',['../interfacecom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1sessioncomparation_1_1DecisionMaker_aa31ead2167b862a26e4e14cb95a038f9.html#aa31ead2167b862a26e4e14cb95a038f9',1,'com.griddynamics.jagger.engine.e1.sessioncomparation.DecisionMaker.makeDecision()'],['../classcom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1sessioncomparation_1_1WorstCaseDecisionMaker_a0eaf1c571b21401b2357908641f3caab.html#a0eaf1c571b21401b2357908641f3caab',1,'com.griddynamics.jagger.engine.e1.sessioncomparation.WorstCaseDecisionMaker.makeDecision()']]],
+  ['metric_5fmarker',['METRIC_MARKER',['../classcom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1collector_1_1MetricCollector_3_01Q_00_01R_00_01E_01_4_a67841b008d1e66893f32ab809bb3af78.html#a67841b008d1e66893f32ab809bb3af78',1,'com::griddynamics::jagger::engine::e1::collector::MetricCollector&lt; Q, R, E &gt;']]],
+  ['metricaggregatorprovider',['MetricAggregatorProvider',['../interfacecom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1collector_1_1MetricAggregatorProvider.html',1,'com::griddynamics::jagger::engine::e1::collector']]],
+  ['metricaggregatorprovider_2ejava',['MetricAggregatorProvider.java',['../MetricAggregatorProvider_8java.html',1,'']]],
   ['metriccalculator',['MetricCalculator',['../classMetricCalculator.html',1,'']]],
   ['metriccalculator_2ejava',['MetricCalculator.java',['../MetricCalculator_8java.html',1,'']]],
-  ['metriccalculator_3c_20r_20_3e',['MetricCalculator&lt; R &gt;',['../interfacecom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1collector_1_1MetricCalculator_3_01R_01_4.html',1,'com::griddynamics::jagger::engine::e1::collector']]]
+  ['metriccalculator_3c_20r_20_3e',['MetricCalculator&lt; R &gt;',['../interfacecom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1collector_1_1MetricCalculator_3_01R_01_4.html',1,'com::griddynamics::jagger::engine::e1::collector']]],
+  ['metriccollector',['MetricCollector',['../classcom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1collector_1_1MetricCollector_3_01Q_00_01R_00_01E_01_4_ab11409ed9dad5205ad4aec60162038d6.html#ab11409ed9dad5205ad4aec60162038d6',1,'com::griddynamics::jagger::engine::e1::collector::MetricCollector&lt; Q, R, E &gt;']]],
+  ['metriccollector_2ejava',['MetricCollector.java',['../MetricCollector_8java.html',1,'']]],
+  ['metriccollector_3c_20q_2c_20r_2c_20e_20_3e',['MetricCollector&lt; Q, R, E &gt;',['../classcom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1collector_1_1MetricCollector_3_01Q_00_01R_00_01E_01_4.html',1,'com::griddynamics::jagger::engine::e1::collector']]],
+  ['metriccollectorprovider_2ejava',['MetricCollectorProvider.java',['../MetricCollectorProvider_8java.html',1,'']]],
+  ['metriccollectorprovider_3c_20q_2c_20r_2c_20e_20_3e',['MetricCollectorProvider&lt; Q, R, E &gt;',['../classcom_1_1griddynamics_1_1jagger_1_1engine_1_1e1_1_1collector_1_1MetricCollectorProvider_3_01Q_00_01R_00_01E_01_4.html',1,'com::griddynamics::jagger::engine::e1::collector']]]
 ];
--- search/files_6d.js
+++ search/files_6d.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['metriccalculator_2ejava',['MetricCalculator.java',['../MetricCalculator_8java.html',1,'']]]
+  ['metricaggregatorprovider_2ejava',['MetricAggregatorProvider.java',['../MetricAggregatorProvider_8java.html',1,'']]],
+  ['metriccalculator_2ejava',['MetricCalculator.java',['../MetricCalculator_8java.html',1,'']]],
+  ['metriccollector_2ejava',['MetricCollector.java',['../MetricCollector_8java.html',1,'']]],
+  ['metriccollectorprovider_2ejava',['MetricCollectorProvider.java',['../MetricCollectorProvider_8java.html',1,'']]]
 ];
