477811a05d6b62a773d2c662ffd05ebc0b49c7f1
dlatnikov <dlatnikov@griddynamics.com>
2013-11-21 10:19:43 +0400

    Docu for distributors and providers with new xsd elements

--- search/groups_67.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['general_20information_20about_20distributors',['General information about distributors',['../group__Main__Distributors__General__group.html',1,'']]],
-  ['general_20information_20about_20invokers',['General information about invokers',['../group__Main__Invokers__General__group.html',1,'']]],
-  ['general_20information_20about_20providers',['General information about providers',['../group__Main__Providers__General__group.html',1,'']]]
-];
--- search/pages_63.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['custom_20components_20for_20jagger',['Custom components for Jagger',['../index.html',1,'']]]
-];
